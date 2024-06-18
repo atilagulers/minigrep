ada03b473cfd9e0356d2cb5af0d41f9e29d38766
Atila Guler <atilagulerdev@gmail.com>
2024-06-18 05:05:05 +0300

    .

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use std::env;
+use std::error::Error;
+use std::fs;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1)
+    });
+
+    println!("Query: {}", config.query);
+    println!("Filename: {}", config.filename);
+
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("{:?}", contents);
+
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Self, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Self { query, filename })
+    }
 }
